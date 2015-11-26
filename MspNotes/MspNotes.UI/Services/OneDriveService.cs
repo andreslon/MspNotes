00364a58871274683dfbf9b6966bdb0bd01b9d92
Andrés Londoño <andreslon@outlook.com>
2015-11-25 19:20:18 -0500

    Instalacion de Onedrive dll

---
+++ MspNotes/MspNotes.UI/Services/OneDriveService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.OneDrive.Sdk;
+
+namespace MspNotes.UI.Services
+{
+    class OneDriveService
+    {
+        public IOneDriveClient oneDriveClient { get; set; }
+        public readonly string[] scopes = new string[] { "onedrive.readwrite", "wl.offline_access", "wl.signin" };
+
+        public OneDriveService()
+        {
+            Initialize();
+        }
+        async private void Initialize()
+        {
+            if (oneDriveClient == null)
+            {
+                oneDriveClient = OneDriveClientExtensions.GetUniversalClient(this.scopes);
+                await oneDriveClient.AuthenticateAsync();
+            }
+
+
+            var childrenPage = await this.oneDriveClient.Drive.Items[""].Children.Request().Expand("thumbnails").GetAsync();
+            var items = childrenPage == null
+              ? new List<Item>()
+              : childrenPage.CurrentPage.Where(item => item.Folder != null || item.Image != null);
+        }
+
+
+
+    }
+}
